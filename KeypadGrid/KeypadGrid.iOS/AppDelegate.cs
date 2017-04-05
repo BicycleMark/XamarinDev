5523283f4313497d92ca7a5722d1d45bcb3c742d
BicycleMark <mark.d.wardell@gmail.com>
2017-04-05 12:57:47 -0400

    This is my Change

--- KeypadGrid/KeypadGrid.iOS/AppDelegate.cs
+++ KeypadGrid/KeypadGrid.iOS/AppDelegate.cs
@@ -10,7 +10,8 @@ namespace KeypadGrid.iOS
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
-    [Register("AppDelegate")]
+    
+	[Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
         //
